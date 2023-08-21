e717b56f1de88183432980d0b7f29e8e95d3541e
Ahmed-Shahi <ahmedshahi038@gmail.com>
2023-08-21 17:48:44 +0500

    not done

---
+++ app.js
@@ -0,0 +1,64 @@
+// function table(a,b,c){
+//     a = +prompt('Enter a Number')
+//     b = +prompt('Enter a starting Number')
+//     c = +prompt('Enter a Ending Number')
+
+//     for(var i=b ; i<=c; i++){
+//      document.write(a,' x ',i,' = ',a*i,'<br>' )
+//     }
+    
+// }
+// table()
+
+// project 1
+// function abc(){
+//     // alert('World')
+
+//     // document.getElementById("para1").innerHTML = 'Hello world'
+// var = 
+//     document.getElementById('an').innerHTML = ''
+// }
+
+// project 1
+
+// project 2
+function word(f){
+document.getElementById('inp').value += f
+}
+function eq (){
+    var a = document.getElementById('inp')
+    a.value= eval(a.value)
+}
+function clr(){
+    document.getElementById('inp').value = ''
+}
+function clra(){
+    
+}
+// project 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
